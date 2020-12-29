f0c2e55b0ed7e5de9fe2b6a11c341958642013e4
CP <1395688079@qq.com>
2020-12-29 14:18:56 +0800

    领悟更改项目名

--- src/main/java/com/maidao/edu/homework/baseexercise/chapter02/Animal.java
+++
@@ -1,7 +0,0 @@
-package com.maidao.edu.homework.baseexercise.chapter02;
-
-public interface Animal {
-
-    void run();
-
-}
--- src/main/java/com/maidao/edu/homework/baseexercise/chapter02/EnumTestExercise.java
+++
@@ -1,7 +0,0 @@
-package com.maidao.edu.homework.baseexercise.chapter02;
-
-public class EnumTestExercise {
-    public enum colour {
-        REd, green, black, white, gray;
-    }
-}
